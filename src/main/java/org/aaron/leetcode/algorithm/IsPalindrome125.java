fb16fee676235d69a6fc9c8fd1eb6e60f2043bd8
Menng <concisesky@gmail.com>
2021-03-20 16:18:33 +0800

    回文数

--- src/main/java/org/aaron/leetcode/algorithm/IsPalindrome125.java
+++ src/main/java/org/aaron/leetcode/algorithm/IsPalindrome125.java
@@ -42,4 +42,30 @@ public class IsPalindrome125 {
 
         return true;
     }
+
+    public boolean isPalindrome3(String s) {
+        int length = s.length();
+        int left = 0;
+        int right = length - 1;
+        while (left < right) {
+            if (!Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+                continue;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
 }
