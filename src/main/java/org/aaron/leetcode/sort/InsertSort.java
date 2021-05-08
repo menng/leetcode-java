5c7071ba04e39e4ec2ffd98dd1584e8d624aef4b
Menng <concisesky@gmail.com>
2021-05-08 14:40:31 +0800

    update

--- src/main/java/org/aaron/leetcode/sort/InsertSort.java
+++ src/main/java/org/aaron/leetcode/sort/InsertSort.java
@@ -17,11 +17,35 @@ public class InsertSort {
         }
     }
 
+    public static void insertSort2(int[] s){
+      for(int i=1; i < s.length; i++) {
+          int value = s[i];
+          int j = i - 1;
+          for(; j >= 0; j--) {
+              if (s[j] > value) {
+                  s[j+1] = s[j];
+              } else {
+                  break;
+              }
+          }
+
+          s[j+1] = value;
+      }
+    }
+
     public static void main(String[] args) {
         int[] s = new int[]{5, 4, 3, 2, 1};
+        int[] t = new int[]{5, 4, 3, 2, 1};
         insertSort(s);
+        insertSort2(t);
         for (int i=0; i < s.length; i++) {
             System.out.println(s[i]);
         }
+
+        System.out.println("--------------");
+        for (int i=0; i < t.length; i++) {
+            System.out.println(t[i]);
+        }
+
     }
 }
---
+++ src/main/java/org/aaron/leetcode/sort/QuickSort.java
@@ -0,0 +1,7 @@
+package org.aaron.leetcode.sort;
+
+public class QuickSort {
+    public static void quickSort(int[] s){
+
+    }
+}
